2b54696a3b70f6f2da6a0723df5f73d94f3fae33
sifrajo <jesseorlanda241102@gmail.com>
2023-04-03 12:52:09 +0700

    template engine

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <a href="/mahasiswa">Mahasiswa</a>
+    |
+    <a href="/genshin">Genshin</a>
+    |
+    <a href="/blog">HOME</a>
+    |
+    <a href="/blog/tentang">TENTANG</a>
+    |
+    <a href="/blog/kontak">KONTAK</a>
+
+</nav>
